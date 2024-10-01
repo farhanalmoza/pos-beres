32875b297f1713187055bfd3bbfff8ee331f1ba6
Farhan <oyisamfarhan@gmail.com>
2024-10-01 13:24:22 +0700

    forgot password

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,65 +1,120 @@
-@extends('layouts.app')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+<!DOCTYPE html>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('password.update') }}">
-                        @csrf
+<html
+  lang="en"
+  class="light-style layout-wide customizer-hide"
+  dir="ltr"
+  data-theme="theme-default"
+  data-template="vertical-menu-template-free">
+  <head>
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
-                        <input type="hidden" name="token" value="{{ $token }}">
+    <title>Reset password</title>
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+    <meta name="description" content="" />
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="{{ asset('/') }}img/favicon/favicon.ico" />
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+      rel="stylesheet" />
 
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+    <link rel="stylesheet" href="{{ asset('/') }}vendor/fonts/boxicons.css" />
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+    <!-- Core CSS -->
+    <link rel="stylesheet" href="{{ asset('/') }}vendor/css/core.css" class="template-customizer-core-css" />
+    <link rel="stylesheet" href="{{ asset('/') }}vendor/css/theme-default.css" class="template-customizer-theme-css" />
+    <link rel="stylesheet" href="{{ asset('/') }}css/demo.css" />
 
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+    <!-- Vendors CSS -->
+    <link rel="stylesheet" href="{{ asset('/') }}vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
 
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
+    <!-- Page CSS -->
+    <!-- Page -->
+    <link rel="stylesheet" href="{{ asset('/') }}vendor/css/pages/page-auth.css" />
 
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
+    <!-- Helpers -->
+    <script src="{{ asset('/') }}vendor/js/helpers.js"></script>
+    <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
+    <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
+    <script src="{{ asset('/') }}js/config.js"></script>
+  </head>
 
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Reset Password') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+  <body>
+    <!-- Content -->
+
+    <div class="container-xxl">
+      <div class="authentication-wrapper authentication-basic container-p-y">
+        <div class="authentication-inner">
+          <!-- Reset Password -->
+          <div class="card">
+            <div class="card-body">
+							@if (session('status'))
+							<div class="alert alert-success" role="alert">
+								{{ session('status') }}
+							</div>
+							@endif
+              <h4 class="mb-2">Reset Password</h4>
+              <form class="mb-3" action="{{ route('password.update') }}" method="POST" autocomplete="off">
+								@csrf
+								<input type="hidden" name="token" value="{{ $token }}">
+								<input type="hidden" name="email" value="{{ $email }}">
+                <div class="mb-3">
+                  <label for="password" class="form-label">Password Baru</label>
+                  <input type="password" class="form-control" id="password" name="password" placeholder="Masukkan password baru" autofocus="">
+									@error('password')
+									<div class="form-text text-danger">
+										{{ $message }}
+									</div>
+									@enderror
                 </div>
+								<div class="mb-3">
+                  <label for="password-confirm" class="form-label">Konfirmasi Password Baru</label>
+                  <input type="password" class="form-control" id="password-confirm" name="password_confirmation" placeholder="Konfirmasi password baru" autofocus="">
+									@error('password_confirmation')
+									<div class="form-text text-danger">
+										{{ $message }}
+									</div>
+									@enderror
+                </div>
+                <button class="btn btn-primary d-grid w-100">Reset Password</button>
+              </form>
             </div>
+          </div>
+          <!-- /Reset Password -->
         </div>
+      </div>
     </div>
-</div>
-@endsection
+
+    <!-- / Content -->
+
+    <!-- Core JS -->
+    <!-- build:js assets/vendor/js/core.js -->
+
+    <script src="{{ asset('/') }}vendor/libs/jquery/jquery.js"></script>
+    <script src="{{ asset('/') }}vendor/libs/popper/popper.js"></script>
+    <script src="{{ asset('/') }}vendor/js/bootstrap.js"></script>
+    <script src="{{ asset('/') }}vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+    <script src="{{ asset('/') }}vendor/js/menu.js"></script>
+
+    <!-- endbuild -->
+
+    <!-- Vendors JS -->
+
+    <!-- Main JS -->
+    <script src="{{ asset('/') }}js/main.js"></script>
+
+    <!-- Page JS -->
+
+    <!-- Place this tag in your head or just before your close body tag. -->
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
+  </body>
+</html>
