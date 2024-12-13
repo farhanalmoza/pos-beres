0837f646dcfebc8cf715121da4052941bfb8c385
Farhan <oyisamfarhan@gmail.com>
2024-12-14 06:17:10 +0700

    dashboard cashier view - store

--- app/Http/Controllers/Cashier/DashboardController.php
+++ app/Http/Controllers/Cashier/DashboardController.php
@@ -3,11 +3,32 @@
 namespace App\Http\Controllers\Cashier;
 
 use App\Http\Controllers\Controller;
+use App\Models\ProductOut;
+use App\Models\Transaction;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function index() {
-        return view('cashier.dashboard.index');
+        $thisMonth = Carbon::now()->month;
+        $thisYear = Carbon::now()->year;
+
+        $data['expensesThisMonth'] = ProductOut::where('store_id', Auth::user()->store_id)
+            ->whereMonth('created_at', $thisMonth)
+            ->whereYear('created_at', $thisYear)
+            ->sum('total_price');
+        $salesThisMonth = Transaction::where('store_id', Auth::user()->store_id)
+            ->whereMonth('created_at', $thisMonth)
+            ->whereYear('created_at', $thisYear)
+            ->sum('total');
+        $data['profitThisMonth'] = $salesThisMonth - $data['expensesThisMonth'];
+        $data['transactionsThisMonth'] = Transaction::where('store_id', Auth::user()->store_id)
+            ->whereMonth('created_at', $thisMonth)
+            ->whereYear('created_at', $thisYear)
+            ->count();
+
+        return view('cashier.dashboard.index', compact('data'));
     }
 }
--- resources/views/cashier/dashboard/index.blade.php
+++ resources/views/cashier/dashboard/index.blade.php
@@ -3,6 +3,33 @@
 
 @section('content')
 <div class="container-xxl flex-grow-1 container-p-y">
-
+  <div class="row">
+    <div class="col-md-3 mb-4">
+      <div class="card card-border-shadow-primary">
+        <div class="card-body text-end">
+          <h5 class="card-title text-danger">Rp. {{ number_format($data['expensesThisMonth']) }}</h5>
+          <div class="card-subtitle text-muted mb-3">Pengeluaran Bulan Ini</div>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-3 mb-4">
+      <div class="card card-border-shadow-primary">
+        <div class="card-body text-end">
+          <h5 class="card-title @if($data['profitThisMonth'] > 0) text-success @else text-danger @endif">
+            Rp. {{ number_format($data['profitThisMonth']) }}
+          </h5>
+          <div class="card-subtitle text-muted mb-3">Keuntungan Bulan Ini</div>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-3 mb-4">
+      <div class="card card-border-shadow-primary">
+        <div class="card-body text-end">
+          <h5 class="card-title">{{ $data['transactionsThisMonth'] }}</h5>
+          <div class="card-subtitle text-muted mb-3">Transaksi Bulan Ini</div>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
 @endsection
